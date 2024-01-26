7f7be9510ad76e27cff544aa71b82b6e4222b2d7
allow <Skarlett@github.com>
2024-01-25 19:14:14 -0600

    undo mistakes

--- flake-modules/deploy-rs.nix
+++ flake-modules/deploy-rs.nix
@@ -16,258 +16,140 @@ with lib;
 let
   cfg = config.deploy;
 
-  nodeSettings = settings:
-    { options = builtins.removeAttrs ["system"] settings.options; };
-
-  clusterSettings =
-    { options = builtins.removeAttrs ["path"] settings.options; };
-
-  deployCluster =
-    {deploy-rs, writeShellScript, signingKey, nodes, uri}:
-      writeShellScript {
-        text = builtins.concatStringSep "\n" (map (node: ''
-          URI=\''${1:-${URI}}
-          LOCAL_KEY=${signingKey} ${deploy-rs}/bin/deploy ${uri}#${node}
-        '') nodes) ;
-      };
-
-  deployWrapper =
-    {deploy-rs, writeShellScript, signingKey}:
-      config.deploy.packages."${config.host.system}".default.overrideAttrs (old: {
-        nativeBuildInputs = [ pkgs.mkWrapper ];
-        installPhase = old.installPhase ++ ''
-          wrapProgram $out/bin/deploy \
-            --set LOCAL_KEY ${config.deploy.settings.signingKey}
-        '';
-      });
-
-
-  clusterModule = backref:
+  profile.options =
   {
-    options = {
-      _cluster = mkOption {
-        type = types.string;
-        default = builtins.tail backref;
-      };
+    path = mkOption {
+      default = null;
+    };
 
-      _backref = mkOption {
-        type = types.listOf types.string;
-        default = backref;
-      };
+    sshOpts = mkOption {
+      type = types.listOf types.string;
+      default = config.deploy.defaultSshOpts;
+    };
 
-      nodes = mkOption {
-        type = types.attrsOf (types.submodule nodes);
-        default = {};
-      };
+    sshUser = mkOption {
+      type = types.string;
+      default = config.deploy.defaultSshUser;
+    };
 
-      settings = mkOption {
-        type = types.attrsOf (types.submodule settingsModule backref );
-        default = config.deploy.settings;
+    user = mkOption {
+      type = types.string;
+      default = "root";
+    };
+
+    sudo = mkOption {
+      type = types.string;
+      default = "sudo -u";
+    };
+
+    magicRollback = mkOption {
+      type = types.bool;
+      default = true;
+    };
+
+    autoRollback = mkOption {
+      type = types.bool;
+      default = true;
+    };
+
+    fastConnection = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    tempPath = mkOption {
+      type = types.path;
+      default = "/tmp";
+    };
+
+    remoteBuild = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    activationTimeout = mkOption {
+      type = types.int;
+      default = 240;
+    };
+
+    confirmTimeout = mkOption {
+      type = types.int;
+      default = 30;
+    };
+  };
+
+  nodes.options = {
+    hostname = mkOption {
+      type = types.string;
+    };
+
+    system = mkOption {
+      type = types.string;
+      example = "aarch64-linux";
+    };
+
+    profileOrder = mkOption {
+      type = types.listOf types.string;
+      default = [];
+    };
+
+    profiles = mkOption {
+      type = types.attrsOf (types.submodule profile);
+      default = {
+        system.path = null;
       };
     };
   };
 
-
-  nodesModule = backref:
-    let
-      lookup = backref ++ ["settings"];
-      cfg = getAttrFromPath lookup config;
-    in
-    {
-      options = {
-        system = mkOption {
-          type = types.string;
-          default = cfg.system;
-        };
-
-        hostname = mkOption {
-          type = types.string;
-        };
-
-        profileOrder = mkOption {
-          type = types.listOf types.string;
-          default = cfg.profileOrder;
-        };
-
-        # profiles/settings
-        profiles = mkOption {
-          type = types.attrsOf (types.submodule (
-            settingsModule backref config
-          ));
-
-          default = {
-            system.path = null;
-          };
-        };
-      };
-
-    };
-
-  settingsSubModule = backref: # ["config" "deploy" "cluster" "settings"]
-    let
-      # ["config" "deploy" "cluster" "settings"]
-      lookup = backref ++ ["settings"];
-      cfg = getAttrFromPath lookup config;
-    in
-  {
-    options =
-    {
-      signingKey = mkOption {
-        type = types.path;
-        default = cfg.signingKey;
-      };
-
-      package = mkOption {
-        type = types.derivation;
-        default = pkgs.callPackage deployWrapper {
-          signingKey = cfg.signingKey;
-        };
-      };
-      #####
-      path = mkOption {
-        type = types.any;
-        default = null;
-      };
-      sshOpts = mkOption {
-        type = types.listOf types.string;
-        default = cfg.sshOpts;
-      };
-      sshUser = mkOption {
-        type = types.string;
-        default = cfg.sshUser;
-      };
-      magicRollback = mkOption {
-        type = types.bool;
-        default = cfg.magicRollback;
-      };
-      autoRollback = mkOption {
-        type = types.bool;
-        default = cfg.autoRollback;
-      };
-      user = mkOption {
-        type = types.string;
-        default = cfg.user;
-      };
-      sudo = mkOption {
-        type = types.string;
-        default = cfg.sudo;
-      };
-      fastConnection = mkOption {
-        type = types.bool;
-        default = cfg.fastConnection;
-      };
-      tempPath = mkOption {
-        type = types.path;
-        default = cfg.tempPath;
-      };
-      remoteBuild = mkOption {
-        type = types.bool;
-        default = cfg.remoteBuild;
-      };
-      activationTimeout = mkOption {
-        type = types.int;
-        default = cfg.activationTimeout;
-      };
-      confirmTimeout = mkOption {
-        type = types.int;
-        default = cfg.confirmTimeout;
-      };
-    };
-  };
 in
+# flake.deploy.defaultSshUser = "storm";
+# flake.deploy.nodes.host.user = "lunarix";
+
 {
   options.deploy = {
     input = mkOption {
       type = types.lazyAttrsOf types.attrs;
     };
 
-    settings = mkOption {
-      type = types.attrsOf (types.submodule
-        (settingsModule ["config" "deploy"])
-      );
-      default = {};
+    defaultSystem = mkOption {
+      type = types.string;
+      default = "x86_64-linux";
     };
 
-    cluster = mkOption {
-      type = types.attrsOf (types.submodule
-        (clusterModule ["config" "deploy" "cluster"] )
-      );
-      default = {};
+    defaultSshOpts = mkOption {
+      type = types.listOf types.string;
+      default = [];
+    };
+
+    defaultSshUser = mkOption {
+      type = types.string;
+      default = "root";
+    };
+
+    defaultUser = mkOption {
+      type = types.string;
+      default = "root";
     };
 
     nodes = mkOption {
-      type = types.attrsOf (types.submodule node-options ["config" "deploy" ] );
+      type = types.attrsOf (types.submodule nodes);
+      default = {};
     };
   };
 
   config = mkIf cfg.enable
   {
-    deploy.settings = {
-      system = mkOptionDefault "x86_64-linux";
-      signingKey = mkOptionDefault "/run/secrets/store-signing-key";
-
-
-      sshOpts = mkOptionDefault [];
-      user = mkOptionDefault "root";
-      sshUser = mkOptionDefault "root";
-      magicRollback = mkOptionDefault true;
-      automaticRollback = mkOptionDefault true;
-      sudo = mkOptionDefault "sudo -u";
-      fastConnection = mkOptionDefault false;
-      tempPath = mkOptionDefault "/tmp";
-      remoteBuild = mkOptionDefault false;
-      activationTimeout = mkOptionDefault 240;
-      confirmTimeout = mkOptionDefault 30;
+    flake.deploy = cfg // {
+      nodes = (builtins.mapAttrs (k: v:
+        v // {
+          path =
+            if v.profiles.system.path != null then
+              v.profiles.system.path
+            else
+              config.deploy.input.lib.${config.deploy.nodes.${k}.system}.activate.nixos
+                self.nixosConfigurations.${k};
+        }
+      ) cfg.nodes);
     };
-
-    flake.deploy =
-      mkMerge [
-        cfg
-
-        ({...}:
-        {
-
-          flake.deploy.clusters =
-            let
-              deploy-lib = config.deploy.input.lib.${config.deploy.system};
-            in
-
-            builtins.mapAttrs (cluster-name: cluster-set:
-              (builtins.removeAttrs [ "package" "signingKey"] cluster-set.settings) //
-              {
-                nodes = builtins.mapAttrs (node-name: node-set:
-                  (builtins.removeAttrs ["system" "package" "signingKey"] node-set.settings) //
-                  {
-                    path =
-                      if v.profiles.system.path != null then
-                        v.profiles.system.path
-                      else
-                        deploy-lib.activate.nixos
-                          self.nixosConfigurations.${node-name};
-
-                  }) cluster-set.nodes
-              }) cfg.clusters;
-
-          flake.deploy.nodes =
-            let
-              deploy-lib = config.deploy.input.lib.${config.deploy.nodes.${node-name}.system};
-            in
-              (builtins.mapAttrs (node-name: node-set:
-                (builtins.removeAttrs ["system" "package" "signingKey"] node-set.settings) //
-                {
-                  path =
-                    if v.profiles.system.path != null then
-                      v.profiles.system.path
-                    else
-                      deploy-lib.activate.nixos
-                        self.nixosConfigurations.${node-name};
-
-                }) cluster-set.nodes
-              ) cfg.clusters
-
-
-        })
-      ];
-
   };
 }
