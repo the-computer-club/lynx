cf8ca12d25ef4e550a47e478b59b72e7722c525b
allow <Skarlett@github.com>
2024-01-27 16:47:29 -0600

    remove deploy-rs input

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     parts.url = "github:hercules-ci/flake-parts";
-    deploy.url = "github:serokell/deploy-rs";
   };
 
   outputs = inputs@{self, parts, ...}:
