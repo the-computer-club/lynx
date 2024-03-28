e54d78307952c8dad752362a38c828623feb19d2
Lunarix <3759687+Skarlett@users.noreply.github.com>
2024-03-28 18:44:00 -0500

    hotfix: broken merge

--- nixos-modules/globals.nix
+++ nixos-modules/globals.nix
@@ -2,23 +2,14 @@
 with lib;
 {
   options.globals = mkOption {
-<<<<<<< HEAD
-    type = lib.types.attrsOf lib.types.anything;
-=======
     type = types.attrsOf types.anything;
->>>>>>> devel
     default = {};
     description = ''
       A set of global variables to be made available to all modules.
     '';
     example = literalExpression
       ''
-<<<<<<< HEAD
-        globals.var-name = ["value"];
-    '';
-=======
       globals.var-name = ["value"];
       '';
->>>>>>> devel
   };
 }
