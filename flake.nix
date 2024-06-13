06876f05915b12030231c5c969e54410409ab329
allow <Skarlett@github.com>
2024-06-13 10:12:18 -0500

    export domains
    (cherry picked from commit 2c693f36308baef0dc49c80a0085f74579e0ee9c)

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
       deploy-rs   = import ./flake-modules/deploy-rs;
       lynx-docs   = import ./flake-modules/lynx-docs;
       flake-guard = import ./flake-modules/flake-guard;
+      domains = import ./flake-modules/domains;
       profile-parts-homexts = import ./flake-modules/profile-parts-homext.nix;
     };
 
