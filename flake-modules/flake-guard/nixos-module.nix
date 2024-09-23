3dd831af031694dd1377ad9d36d48b6c827ab6e7
allow <Skarlett@github.com>
2024-09-23 12:09:23 -0500

    wtf v5

--- flake-modules/flake-guard/nixos-module.nix
+++ flake-modules/flake-guard/nixos-module.nix
@@ -124,7 +124,7 @@ in
             ({
               found = lib.mkForce true;
               privateKeyFile =
-                (safeHead (filter (x: x == null)
+                safeHead ((filter (x: x == null)
                   (map (x: if (x != null) then x else null) ([
                     (deriveSecret (lib.traceValSeqN 3 network).privateKeyFile)
                     (deriveSecret (lib.traceValSeqN 3 network).secretsLookup)
